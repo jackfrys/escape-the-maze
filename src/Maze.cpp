e55aab6d7c664b83ed9f7a50e80399b18236a0a6
Jack Frysinger <frysinger.j@husky.neu.edu>
2018-11-08 21:05:16 -0500

    Fix delete mismatches

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -35,10 +35,10 @@ Maze::Maze(int newSize) {
 Maze::~Maze() {
   // Free up space
   for(int i = 0; i < size; i++) {
-    delete[] cells[i];
+    delete [] cells[i];
   }
-  delete cells;
-  delete keys;
+  delete [] cells;
+  delete [] keys;
 }
 
 void Maze::generateMaze(int sx, int sy) {
