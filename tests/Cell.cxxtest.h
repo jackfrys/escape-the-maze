b183f8c8225a325daa8e41ca00df3a007ab77ad6
kevin altschuler <kevin@kevins-MacBook-Pro.local>
2018-12-04 13:12:02 -0500

    cell testing

--- tests/Cell.cxxtest.h
+++ tests/Cell.cxxtest.h
@@ -16,6 +16,23 @@ public:
     TS_ASSERT(!c.notVisited());
   }
 
+  void test_walls_and_move() {
+  	Cell c;
+  	TS_ASSERT_EQUALS(c.canMove(2), false);
+  	TS_ASSERT_EQUALS(c.canMove(3), false);
+  	c.breakWall(2);
+  	TS_ASSERT_EQUALS(c.canMove(2), true);
+  	TS_ASSERT_EQUALS(c.canMove(3), false);
+
+  	TS_ASSERT_EQUALS(c.canMove(0), false);
+  	TS_ASSERT_EQUALS(c.canMove(1), false);
+  	c.breakOppWall(1);
+  	TS_ASSERT_EQUALS(c.canMove(0), true);
+  	TS_ASSERT_EQUALS(c.canMove(1), false);
+
+  	TS_ASSERT_EQUALS(c.canMove(), false);
+  }
+
 };
 
 #endif
