fdca32b06e4c223c0514fe98ee8ef8f6e15f3617
Jonah-Min <jonahm2472@gmail.com>
2018-12-04 13:08:09 -0500

    Separate Key and Guard into classes

---
+++ src/Guard.h
@@ -0,0 +1,12 @@
+#ifndef GUARD_H
+#define GUARD_H
+
+#include "Util.h"
+#include <iostream>
+
+class Guard {
+public:
+  Posn p;
+};
+
+#endif
---
+++ src/Key.h
@@ -0,0 +1,13 @@
+#ifndef KEY_H
+#define KEY_H
+
+#include <iostream>
+#include "Util.h"
+
+class Key {
+public:
+  Posn p;
+  bool found;
+};
+
+#endif
